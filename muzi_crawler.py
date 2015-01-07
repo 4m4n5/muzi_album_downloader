a29624b77f2a198f8242a4b2e0145a933b86c8ed
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-01-07 16:56:15 +0530

    Handled some exceptions

--- muzi_crawler.py
+++ muzi_crawler.py
@@ -19,11 +19,12 @@ source_code = requests.get(url)
 
 json_data = source_code.json()
 
-name_of_album = json_data[u'name']
+album = json_data[u'name']
+names_of_album = [album, album.replace(' [Deluxe]','')]
 
 artist = json_data[u'band']
-
-print 'Name of the album : ' + name_of_album
+for name_of_album in names_of_album:
+    print 'Possible Names of the album : ' + name_of_album
 print 'Name of Band : ' + artist
 
 def download(down_link, name):
@@ -39,17 +40,18 @@ for i in range(0, number_of_songs):
     print track
     base_download_url = 'https://music.sdslabs.co.in/English/'
     # after track id and before track
-    seperators = [' ', ' - ', '. ', ' . ', '. '+artist+' - ']
+    seperators = [' ', ' - ', '. ', ' . ', '. '+artist+' - ', ' '+artist+' - ']
     # check song extension
     song_extensions = ['.mp3', '.m4a']
     # include or exclude 0
     numbers = ['0', '']
-    for sep in seperators:
-        for ext in song_extensions:
-            if(int(track_id)<=9):
-                for num in numbers:
-                    download_url = base_download_url + artist + '/' + name_of_album + '/' + num + track_id + sep + track + ext
+    for name_of_album in names_of_album:
+        for sep in seperators:
+            for ext in song_extensions:
+                if(int(track_id)<=9):
+                    for num in numbers:
+                        download_url = base_download_url + artist + '/' + name_of_album + '/' + num + track_id + sep + track + ext
+                        download(download_url, track+'.mp3')
+                else:
+                    download_url = base_download_url + artist + '/' + name_of_album + '/' + track_id + sep + track + ext
                     download(download_url, track+'.mp3')
-            else:
-                download_url = base_download_url + artist + '/' + name_of_album + '/' + track_id + sep + track + ext
-                download(download_url, track+'.mp3')
